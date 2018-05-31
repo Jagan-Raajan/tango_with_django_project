7473bcdd0cec828e75113f0887508e4e4f65a514
jagan-raajan <jagan.ni31@gmail.com>
2018-05-31 14:41:36 +0530

    Checking if this works

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey dumbfuck!!!!")
+    return HttpResponse("Rango says hey dumbfuck!!!!<br/> <a href='/rango/about/'>About</a>" )
+
+def about(request):
+    return HttpResponse("Rango says here is the fucking about pagggeee!!! <a href='/rango/'>Index</a>")
